78dfb65c0c87808393f137564680e5024a264ddb
Carlos Rodrigues <cerf@furg.br>
2025-04-21 10:38:49 -0300

    refactor: streamline argument handling and error management in main function

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,63 +1,21 @@
+mod args;
 mod binary_search;
+use args::get_args;
 use binary_search::binary_search;
 
-fn main() {
-    let time = std::time::Instant::now();
-    let args: Vec<String> = std::env::args().collect();
-    let mut size: Option<i64> = None;
-    let mut target: Option<i64> = None;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (arg_array_size, arg_target) = get_args()?;
 
-    let mut i = 1;
-    while i < args.len() {
-        match args[i].as_str() {
-            "--size" | "-s" => {
-                if i + 1 < args.len() {
-                    size = Some(args[i + 1].parse().unwrap_or_else(|_| {
-                        println!("The value for --size must be a valid number");
-                        std::process::exit(1);
-                    }));
-                    i += 2;
-                } else {
-                    println!("--size requires a value");
-                    std::process::exit(1);
-                }
-            }
-            "--target" | "-t" => {
-                if i + 1 < args.len() {
-                    target = Some(args[i + 1].parse::<i64>().unwrap_or_else(|e| {
-                        println!("--target: {:?}", e);
-                        println!("The value for --target must be a valid number");
-                        std::process::exit(1);
-                    }));
-                    i += 2;
-                } else {
-                    println!("--target requires a value");
-                    std::process::exit(1);
-                }
-            }
-            arg if arg.starts_with("-") => {
-                println!("Unknown option: {}", arg);
-                std::process::exit(1);
-            }
-            _ => {
-                i += 1;
-            }
-        }
+    if arg_array_size < 0 {
+        return Err("Size must be non-negative".into());
     }
 
-    let array_size = size.unwrap_or_else(|| {
-        println!("--size argument is required");
-        std::process::exit(1);
-    });
-    let target = target.unwrap_or_else(|| {
-        println!("--target argument is required");
-        std::process::exit(1);
-    });
+    let array: Vec<i64> = (0..(arg_array_size + 1)).collect();
 
-    let array: Vec<i64> = (0..(array_size + 1)).collect();
+    let time = std::time::Instant::now();
 
-    let index = binary_search(&array, target).unwrap_or_else(|| {
-        let message = format!("The number {} is not in the array", target);
+    let index = binary_search(&array, arg_target).unwrap_or_else(|| {
+        let message = format!("The number {} is not in the array", arg_target);
         println!("{}", message);
         std::process::exit(1);
     });
@@ -67,4 +25,5 @@ fn main() {
     );
 
     println!("Elapsed time: {:.2?}", time.elapsed());
+    Ok(())
 }
